dfbec39cef2b797f37685363c99fee3d7a15af5e
Yuesong Xie <cedricxie@pal-nat186-154-161.itap.purdue.edu>
2016-03-22 07:55:54 -0400

    Revert "Added MEMOSA codes in src folder"
    This reverts commit df696ddf81519354248b339e5958bb6a53d4a93a.

--- src/MEMOSA/lib/util.py
+++
@@ -1,3 +0,0 @@
-print "\nWARNING: The UQ functions have been removed from MEMOSA."
-print "dump_hdf5 is in the 'puqutil' module. To import dump_hdf5"
-print "you should do 'from puqutil import dump_hdf5'\n"
